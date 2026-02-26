88209eff6552196d6ba418eda299adecf3551172
thu789987 <113039001+thu789987@users.noreply.github.com>
2026-02-26 06:51:14 +0000

    update

--- components/ScrollPlayVideo.tsx
+++ components/ScrollPlayVideo.tsx
@@ -68,7 +68,7 @@ export const ScrollPlayVideo = ({
   return (
     <div className={className} style={{ width: '100%', position: 'relative' }}>
       {/* Container rỗng cho thư viện chèn Video vào */}
-      <div ref={containerRef} style={{ width: '100%', height: '100vh' }}></div>
+      <div ref={containerRef} style={{ width: '100%', height: '300vh' }}></div>
     </div>
   );
 };
