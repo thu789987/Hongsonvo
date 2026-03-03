3a9ca3e1aee7738ab9ecdf11bcc80f62852b9096
thu789987 <113039001+thu789987@users.noreply.github.com>
2026-03-03 14:22:50 +0000

    Update

--- components/ScrollPlayVideo.tsx
+++ components/ScrollPlayVideo.tsx
@@ -67,8 +67,7 @@ export const ScrollPlayVideo = ({
 
 return (
     // THẺ CHA: Nhận class từ Plasmic, chiều cao 100% để bám sát thẻ bọc ngoài
-    <div className={className} style={{ width: '100%', height: '100%', position: 'relative' }}>
-      
+      <div className={`animate-banner ${className || ''}`} style={{ width: '100%', height: '100%', position: 'relative' }}>      
       {/* THẺ CON: Đổi 300vh thành 100% */}
       <div ref={containerRef} style={{ width: '100%', height: '100%' }}></div>
 
