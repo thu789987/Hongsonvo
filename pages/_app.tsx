316193350a197080a936b5cca263cf775b035845
thu789987 <113039001+thu789987@users.noreply.github.com>
2026-05-07 08:31:46 +0000

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head'; // 1. Bắt buộc phải import Head từ next/head
 import { CursorProvider } from '../components/CursorContext'; 
 import { CustomCursor } from '../components/CustomCursor';
 
@@ -6,6 +7,11 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     // Cái này sẽ tự động bọc mọi trang (index, about, contact...) trên web của bạn
     <CursorProvider>
+      {/* 2. Đặt thẻ meta vào bên trong Head */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </Head>
+
       <CustomCursor />
       <Component {...pageProps} />
     </CursorProvider>
