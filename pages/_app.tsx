8e88cde35eaf958624dceee0a51c0500d138b173
thu789987 <113039001+thu789987@users.noreply.github.com>
2026-06-05 04:17:01 +0000

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,6 @@ import { CustomCursor } from "../components/CustomCursor";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* 🎯 BẮN TỈA DIỆN RỘNG: Ép TẤT CẢ mọi phần tử nếu chứa biến lỗi này phải đổi thành Koulen */}
-      <style jsx global>{`
-        * {
-          --mixin-9Kwp1_irI7wZ_font-family: 'Koulen', sans-serif !important;
-        }
-      `}</style>
-
       <CursorProvider>
         <Head>
           <meta
@@ -21,11 +14,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
           />
           
-          {/* 🌐 TẢI FONT TRUYỀN THỐNG: Nhúng trực tiếp link Google Fonts vào HTML */}
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link href="https://fonts.googleapis.com/css2?family=Koulen&display=swap" rel="stylesheet" />
-
           <link
             rel="preload"
             as="video"
