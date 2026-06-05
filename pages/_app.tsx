3b0cbb5b594a855cd05feeacb12a179e78575b1a
thu789987 <113039001+thu789987@users.noreply.github.com>
2026-06-05 04:02:06 +0000

    Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
-import "../components/plasmic/son_vo/plasmic.css"; // plasmic-import: bLa1shfC4noziDsvmxjKJF/projectcss
-import { Koulen } from "next/font/google";
+import "../components/plasmic/son_vo/plasmic.css"; 
+import { Koulen } from "next/font/google"; // 👈 Nhập font
 import type { AppProps } from "next/app";
 import Head from "next/head"; 
 import { CursorProvider } from "../components/CursorContext";
 import { CustomCursor } from "../components/CustomCursor";
 
+// 👈 Khởi tạo font Koulen
 const fontKoulen = Koulen({
   weight: "400",
   subsets: ["khmer", "latin"],
@@ -14,13 +15,10 @@ const fontKoulen = Koulen({
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* CÔNG TẮC THÔNG MINH: Chỉ nhắm vào những thẻ được Plasmic gắn tên Koulen */}
+      {/* 👇 BÙA CHÚ ÉP FONT: Đè bẹp biến lỗi của Plasmic */}
       <style jsx global>{`
-        [style*="Koulen"], 
-        [style*="koulen"], 
-        .plasmic_font_Koulen,
-        .plasmic_font_koulen {
-          font-family: ${fontKoulen.style.fontFamily} !important;
+        :root, .plasmic_default_styles {
+          --mixin-9Kwp1_irI7wZ_font-family: ${fontKoulen.style.fontFamily} !important;
         }
       `}</style>
 
@@ -30,7 +28,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             name="viewport"
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
           />
-
           <link
             rel="preload"
             as="video"
